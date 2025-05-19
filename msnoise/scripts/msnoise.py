6d03ccfa67223fa85fbf9021e02017979d372775
BRENOT Laure S. <133853397+LaureBrenot@users.noreply.github.com>
2025-05-18 23:46:18 -0800

    Update msnoise.py
    add batch to wct

--- msnoise/scripts/msnoise.py
+++ msnoise/scripts/msnoise.py
@@ -1133,19 +1133,20 @@ def dvv_compute_dvv(ctx):
 
 @dvv.command(name='compute_wct')
 @click.pass_context
-def dvv_compute_wct(ctx):
+@click.option('-b', '--batch-size', default=5, help='Number of jobs to process in each batch', type=int)
+def dvv_compute_wct(ctx, batch_size):
     """Computes the wavelet jobs based on the new STACK data"""
     from ..s08compute_wct import main
     threads = ctx.obj['MSNOISE_threads']
     delay = ctx.obj['MSNOISE_threadsdelay']
     loglevel = ctx.obj['MSNOISE_verbosity']
     if threads == 1:
-        main(loglevel=loglevel)
+        main(loglevel=loglevel, batch_size=batch_size)
     else:
         from multiprocessing import Process
         processes = []
         for i in range(threads):
-            p = Process(target=main, kwargs={"loglevel": loglevel})
+            p = Process(target=main, kwargs={"loglevel": loglevel,"batch_size": batch_size})
             p.start()
             processes.append(p)
             time.sleep(delay)
